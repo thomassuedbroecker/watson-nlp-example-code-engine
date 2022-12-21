5090697dae695be8e20670c4ea9dbabb1c6b5d74
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-12-21 16:54:33 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,234 @@
-# watson-nlp-example-code-engine
-This is example how to use Watson NLP based on the official example documentation.
+# Run Watson NLP for Embed with Code Engine on IBM Cloudr
+
+This is example how to use Watson NLP based on the official example documentation:[IBM Watson Libraries for Embed](https://www.ibm.com/docs/en/watson-libraries?topic=watson-natural-language-processing-library-embed-home).
+
+### Step 1: Clone the example project to your local computer
+
+```sh
+git clone https://github.com/thomassuedbroecker/watson-nlp-example-code-engine
+cd watson-nlp-example-code-engine/code
+```
+
+### Step 2:  Set your IBM_ENTITLEMENT_KEY in the `.env` file
+
+```sh
+cat .env-template > .env
+```
+
+Edit the `.env` file.
+
+```sh
+# used as 'environment' variables
+IBMCLOUD_ENTITLEMENT_KEY="YOUR_KEY"
+IBMCLOUD_APIKEY="YOUR_KEY"
+CR_NAMESPACE="custom-watson-nlp-YOUR_NAME"
+CE_EMAIL="YOUR_EMAIL"
+CE_PROJECT_NAME="custom-watson-nlp-YOURNAME"
+```
+
+### Step 3: Execute the `run-watson-nlp-with-docker.sh` bash script
+
+```sh
+sh run-watson-nlp-with-code-engine.sh
+```
+
+* Example output:
+
+```sh
+# ******
+# Create custom Watson NLP container imager 
+# ******
+
+
+# ******
+# Connect to IBM Cloud Container Image Registry: cp.icr.io/cp/ai
+# ******
+
+CONTAINER_ENTITLEMENT_KEY: XXXX
+
+WARNING! Using --password via the CLI is insecure. Use --password-stdin.
+Login Succeeded
+
+# ******
+# List model array content
+# ******
+
+Model 0 : watson-nlp_syntax_izumo_lang_en_stock:1.0.7
+Model 1 : watson-nlp_syntax_izumo_lang_fr_stock:1.0.7
+
+# ******
+# Download the models
+# ******
+
+# 1. Run a container in an interactive mode to set the permissions
+# 2. Put models into the file share
+Archive:  /app/model.zip
+  inflating: config.yml              
+1 watson-nlp-models cp.icr.io/cp/ai/watson-nlp_syntax_izumo_lang_en_stock:1.0.7
+Archive:  /app/model.zip
+  inflating: config.yml              
+2 watson-nlp-models cp.icr.io/cp/ai/watson-nlp_syntax_izumo_lang_fr_stock:1.0.7
+
+# ******
+# Create container image
+# ******
+
+Image name: watson-nlp-runtime-with-models
+[+] Building 0.1s (7/7) FINISHED                                                
+ => [internal] load build definition from Dockerfile                       0.0s
+ => => transferring dockerfile: 126B                                       0.0s
+ ...
+ => => writing image sha256:da130d25dbcf3eaa5d8ad7f47c6d4fdb9e460ae2de931  0.0s
+ => => naming to docker.io/library/watson-nlp-runtime-with-models:1.0.0    0.0s
+
+Use 'docker scan' to run Snyk tests against images to find vulnerabilities and learn how to fix them
+
+# ******
+# Upload image to IBM Cloud container registry 
+# ******
+
+
+# ******
+# Log in to IBM Cloud
+# ******
+
+API endpoint: https://cloud.ibm.com
+Region: us-south
+Authenticating...
+OK
+
+Targeted account XXX
+
+                   
+API endpoint:      https://cloud.ibm.com
+Region:            us-south
+User:              XXXX
+Account:           XXXX
+Resource group:    No resource group targeted, use 'ibmcloud target -g RESOURCE_GROUP'
+CF API endpoint:   
+Org:               
+Space:             
+Targeted resource group default
+
+
+                   
+API endpoint:      https://cloud.ibm.com
+Region:            us-south
+User:              XXXX
+Account:           XXXX
+Resource group:    default
+CF API endpoint:   
+Org:               
+Space:             
+Switched to region us-south
+
+
+                   
+API endpoint:      https://cloud.ibm.com
+Region:            us-south
+User:              XXXX
+Account:           XXXX
+Resource group:    default
+CF API endpoint:   
+Org:               
+Space:             
+
+# ******
+# Configure IBM Cloud Registry
+# ******
+
+The region is set to 'us-south', the registry is 'us.icr.io'.
+
+OK
+Adding namespace 'custom-watson-nlp-tsued' in resource group 'default' for account XXXX Account in registry us.icr.io...
+
+The requested namespace is already owned by your account.
+
+OK
+Logging 'docker' in to 'us.icr.io'...
+Logged in to 'us.icr.io'.
+
+OK
+Container image: us.icr.io/custom-watson-nlp-tsued/watson-nlp-runtime-with-models:1.0.0
+The push refers to repository [us.icr.io/custom-watson-nlp-tsued/watson-nlp-runtime-with-models]
+2de7f9fb3378: Pushed 
+...
+9aaca8eae7c0: Pushed 
+1.0.0: digest: sha256:8ad1b4bdd6a89b0686c48fc2325900018a1ddfec0ffb1689457b5c433300d61c size: 4290
+
+# ******
+# Create Code Engine project
+# ******
+
+**********************************
+ Create Code Engine project: custom-watson-nlp-tsued
+**********************************
+Targeted resource group default
+                 
+API endpoint:      https://cloud.ibm.com
+Region:            us-south
+User:              XXXX
+Account:           XXXX
+Resource group:    default
+CF API endpoint:   
+Org:               
+Space:             
+Switched to region us-south
+
+
+                   
+API endpoint:      https://cloud.ibm.com
+Region:            us-south
+User:              XXXX
+Account:           XXXX
+Resource group:    default
+CF API endpoint:   
+Org:               
+Space:             
+
+Creating project 'custom-watson-nlp-tsued'...
+ID for project 'custom-watson-nlp-tsued' is 'b879a212-XXXX'.
+Waiting for project 'custom-watson-nlp-tsued' to be active...
+Now selecting project 'custom-watson-nlp-tsued'.
+OK
+**********************************
+ Configure IBM Cloud Container Registry Access (us.icr.io) for (custom-watson-nlp-tsued)
+**********************************
+**********************************
+ Create Code Engine project: custom-watson-nlp-tsued
+**********************************
+API key: 
+Creating API key cliapikey_custom-watson-nlp-tsued under 641ebfXXXXXXb45279e as XXXXX...
+OK
+API key cliapikey_custom-watson-nlp-tsued was created
+Successfully save API key information to cli_key.json
+Creating image registry access secret 'custom.watson.nlp.cr.sec'...
+OK
+
+# ******
+# Create Code Engine application custom-watson-nlp-application
+# ******
+
+Creating application 'custom-watson-nlp-application'...
+Configuration 'custom-watson-nlp-application' is waiting for a Revision to become ready.
+Ingress has not yet been reconciled.
+Waiting for load balancer to be ready.
+Run 'ibmcloud ce application get -n custom-watson-nlp-application' to check the application status.
+OK
+
+Set CE_APPLICATION_NAME URL: https://custom-watson-nlp-application.wqtmqy9e03u.us-south.codeengine.appdomain.cloud
+
+# ******
+# Verify the custom image
+# ******
+
+
+# ******
+# Verify the application custom-watson-nlp-application
+# ******
+
+{"text":"This is a test sentence.", "producerId":{"name":"Izumo Text Processing", "version":"0.0.1"}, "tokens":[{"span":{"begin":0, "end":4, "text":"This"}, 
+...
+"features":[]}], "sentences":[{"span":{"begin":0, "end":24, "text":"This is a test sentence."}}], "paragraphs":[{"span":{"begin":0, "end":24, "text":"This is a test sentence."}}]}
+```
+
---
+++ code/run-watson-nlp-with-code-engine.sh
@@ -0,0 +1,277 @@
+#!/bin/bash
+
+# **************** Global variables
+source ./.env
+
+######### Watson NLP information ##############
+# Information on https://www.ibm.com/docs/en/watson-libraries?topic=containers-run-docker-run
+IMAGE_REGISTRY="cp.icr.io/cp/ai"
+RUNTIME_IMAGE="watson-nlp-runtime"
+WATSON_NLP_TAG="1.0.20"
+export MODELS="${MODELS:-"watson-nlp_syntax_izumo_lang_en_stock:1.0.7,watson-nlp_syntax_izumo_lang_fr_stock:1.0.7"}"
+IFS=',' read -ra models_arr <<< "${MODELS}"
+MODEL_CONTAINERS_NAME=watson-nlp-models
+TEMP_MODEL_DIR=models
+DOWNLOAD_IMAGE=alpine
+
+######### Create custom Watson NLP image ##############
+CUSTOM_WATSON_NLP_IMAGE_NAME=watson-nlp-runtime-with-models
+CUSTOM_TAG=1.0.0
+
+######### IBM Cloud Container registry for custom image ##############
+CR_CUSTOM_REGISTRY_URL=us.icr.io
+#CR_NAMESPACE="custom-watson-nlp-YOURNAME"
+
+######### IBM Cloud configuration ##############
+RESOURCE_GROUP="default"
+REGION="us-south"
+
+######### IBM Cloud Code Engine #############
+#CE_PROJECT_NAME="custom-watson-nlp-YOURNAME"
+CE_SECRET_NAME="custom.watson.nlp.cr.sec"
+#CE_EMAIL="your@email"
+CE_APPLICATION_NAME="custom-watson-nlp-application"
+CE_ACCEPT_LICENSE="true"
+
+# **********************************************************************************
+# Functions definition
+# **********************************************************************************
+
+############# Custom container related ##############
+
+function connectToModelsInIBMContainerRegistry () {
+    echo ""
+    echo "# ******"
+    echo "# Connect to IBM Cloud Container Image Registry: $IMAGE_REGISTRY"
+    echo "# ******"
+    echo ""
+    echo "CONTAINER_ENTITLEMENT_KEY: $CONTAINER_ENTITLEMENT_KEY"
+    echo ""
+    docker login cp.icr.io --username cp --password $CONTAINER_ENTITLEMENT_KEY
+}
+
+function listModelArray () {
+
+    echo ""
+    echo "# ******"
+    echo "# List model array content"
+    echo "# ******"
+    echo ""
+
+    i=0
+    for model in "${models_arr[@]}"
+    do
+      echo "Model $i : $model"
+      i=$((i+1))
+    done
+}
+
+function downloadTheModels() {
+    
+    echo ""
+    echo "# ******"
+    echo "# Download the models"
+    echo "# ******"
+    echo ""
+
+    mkdir $(pwd)/$TEMP_MODEL_DIR
+
+    echo "# 1. Run a container in an interactive mode to set the permissions"
+    docker run --rm --name $MODEL_CONTAINERS_NAME -it -v "$(pwd)/$TEMP_MODEL_DIR":/model_data $DOWNLOAD_IMAGE chmod 777 /model_data
+
+    echo "# 2. Put models into the file share"
+    i=0
+    for model in "${models_arr[@]}"
+    do
+        docker run --rm --name $MODEL_CONTAINERS_NAME -it -v "$(pwd)/models":/app/models -e ACCEPT_LICENSE=true $IMAGE_REGISTRY/$model
+        i=$((i+1))
+        echo "$i $MODEL_CONTAINERS_NAME $IMAGE_REGISTRY/$model"
+    done
+}
+
+function createCustomContainerImageLocally () {
+    echo ""
+    echo "# ******"
+    echo "# Create container image"
+    echo "# ******"
+    echo ""
+    echo "Image name: $CUSTOM_WATSON_NLP_IMAGE_NAME"
+    docker build --build-arg TAG=$WATSON_NLP_TAG ./ -t "$CUSTOM_WATSON_NLP_IMAGE_NAME":"$CUSTOM_TAG"
+}
+
+############# Code Engine related ############
+
+function loginIBMCloud() {
+
+    echo ""
+    echo "# ******"
+    echo "# Log in to IBM Cloud"
+    echo "# ******"
+    echo ""
+
+    ibmcloud login --apikey $IBMCLOUD_APIKEY
+    ibmcloud target -g $RESOURCE_GROUP
+    ibmcloud target -r $REGION
+}
+
+function configureIBMCloudRegistry () {
+
+    echo ""
+    echo "# ******"
+    echo "# Configure IBM Cloud Registry"
+    echo "# ******"
+    echo ""
+
+    ibmcloud cr region-set $CR_CUSTOM_REGISTRY_URL
+    ibmcloud cr namespace-add $CR_NAMESPACE
+    ibmcloud cr login
+
+}
+
+function uploadCustomImageToIBMCloudRegistry () {
+    # Tag the image
+    echo "Container image: ${CR_CUSTOM_REGISTRY_URL}/${CR_NAMESPACE}/$CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG"
+    docker tag $CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG ${CR_CUSTOM_REGISTRY_URL}/${CR_NAMESPACE}/$CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG
+
+    # Push the image
+    docker push ${CR_CUSTOM_REGISTRY_URL}/${CR_NAMESPACE}/$CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG
+}
+
+function createCE_Project () {
+  echo "**********************************"
+  echo " Create Code Engine project: $CE_PROJECT_NAME" 
+  echo "**********************************"
+
+  ibmcloud target -g $RESOURCE_GROUP
+  ibmcloud target -r $REGION
+
+  ibmcloud ce project create --name $CE_PROJECT_NAME 
+}
+
+function setupCE_CRenv() {
+
+   echo "**********************************"
+   echo " Configure IBM Cloud Container Registry Access ($CR_CUSTOM_REGISTRY_URL) for ($CE_PROJECT_NAME)" 
+   echo "**********************************"
+   
+   IBMCLOUDCLI_KEY_NAME="cliapikey_$CE_PROJECT_NAME"
+   IBMCLOUDCLI_KEY_DESCRIPTION="CLI APIkey $IBMCLOUDCLI_KEY_NAME"
+   CLIKEY_FILE="cli_key.json"
+   USERNAME="iamapikey"
+
+   echo "**********************************"
+   echo " Create Code Engine project: $CE_PROJECT_NAME" 
+   echo "**********************************"
+   
+   RESULT=$(ibmcloud iam api-keys | grep '$IBMCLOUDCLI_KEY_NAME' | awk '{print $1;}' | head -n 1)
+   echo "API key: $RESULT"
+   if [[ $RESULT == $IBMCLOUDCLI_KEY_NAME ]]; then
+        echo "*** The Cloud API key '$IBMCLOUDCLI_KEY_NAME' already exists !"
+        echo "*** The script 'ce-install-application.sh' ends here!"
+        echo "*** Review your existing API keys 'https://cloud.ibm.com/iam/apikeys'."
+        exit 1
+   fi
+
+   ibmcloud iam api-key-create $IBMCLOUDCLI_KEY_NAME -d "My IBM CLoud CLI API key for project $PROJECT_NAME" --file $CLIKEY_FILE
+   CLIAPIKEY=$(cat $CLIKEY_FILE | grep '"apikey":' | awk '{print $2;}' | sed 's/"//g' | sed 's/,//g' )
+   #echo $CLIKEY
+   rm -f $CLIKEY_FILE
+
+   ibmcloud ce registry create --name $CE_SECRET_NAME \
+                               --server $CR_CUSTOM_REGISTRY_URL \
+                               --username $USERNAME \
+                               --password $CLIAPIKEY \
+                               --email $CE_EMAIL
+}
+
+function createCE_App() {
+
+    echo ""
+    echo "# ******"
+    echo "# Create Code Engine application $CE_APPLICATION_NAME"
+    echo "# ******"
+    echo ""
+
+    ibmcloud ce application create --name $CE_APPLICATION_NAME \
+                                   --image "${CR_CUSTOM_REGISTRY_URL}/${CR_NAMESPACE}/$CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG" \
+                                   --cpu "1" \
+                                   --memory "2G" \
+                                   --port 8080 \
+                                   --registry-secret "$CE_SECRET_NAME" \
+                                   --env ACCEPT_LICENSE="$CE_ACCEPT_LICENSE" \
+                                   --max-scale 1 \
+                                   --min-scale 1 
+                                       
+    CE_APPLICATION_NAME_URL=$(ibmcloud ce application get --name "$CE_APPLICATION_NAME" -o url)
+    echo "Set CE_APPLICATION_NAME URL: $CE_APPLICATION_NAME_URL"
+}
+
+############# Verify Custom Watson NLP Image ############
+
+function verifyRunningApplication () {
+    
+    echo ""
+    echo "# ******"
+    echo "# Verify the application $CE_APPLICATION_NAME"
+    echo "# ******"
+    echo ""
+
+    curl -X POST "$CE_APPLICATION_NAME_URL/v1/watson.runtime.nlp.v1/NlpService/SyntaxPredict" \
+                -H "accept: application/json" \
+                -H "grpc-metadata-mm-model-id: syntax_izumo_lang_en_stock" \
+                -H "content-type: application/json" \
+                -d '{ "rawDocument": { "text": "This is a test sentence." }}'
+}
+
+#**********************************************************************************
+# Execution
+# *********************************************************************************
+
+echo ""
+echo "# ******"
+echo "# Create custom Watson NLP container imager "
+echo "# ******"
+echo "" 
+
+connectToModelsInIBMContainerRegistry
+
+listModelArray
+
+downloadTheModels
+
+createCustomContainerImageLocally
+
+echo ""
+echo "# ******"
+echo "# Upload image to IBM Cloud container registry "
+echo "# ******"
+echo "" 
+
+loginIBMCloud
+
+configureIBMCloudRegistry
+
+uploadCustomImageToIBMCloudRegistry
+
+echo ""
+echo "# ******"
+echo "# Create Code Engine project"
+echo "# ******"
+echo "" 
+
+createCE_Project
+
+setupCE_CRenv
+
+createCE_App
+
+echo "" 
+echo "# ******"
+echo "# Verify the custom image"
+echo "# ******"
+echo "" 
+
+verifyRunningApplication
+
+
+
